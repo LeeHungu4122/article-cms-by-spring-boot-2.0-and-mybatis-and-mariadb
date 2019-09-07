016baa23977bbbaf298b86d91048fd79e4ae9b7e
unknown <LeeHungu4122>
2019-09-07 17:46:31 +0900

    articleService, articleServiceImple, ArticleDto 생성 및 세팅, getList() 함수를 구현(가짜 데이터로 리턴하도록 임시로 해놓은 버전)

--- src/main/java/com/sbs/starter/controller/ArticleController.java
+++ src/main/java/com/sbs/starter/controller/ArticleController.java
@@ -1,12 +1,29 @@
 package com.sbs.starter.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.sbs.starter.dto.Article;
+import com.sbs.starter.service.ArticleService;
+
+import jline.internal.Log;
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j // log.info(..) 가 사용가능한 명령어, 디버깅 용도.
 public class ArticleController {
+	@Autowired
+	ArticleService articleService;
+	
 	@RequestMapping("/article/list")
 	public String showList() {
+		List<Article> list = articleService.getList();
+		
+		Log.info("list : " + list);
+		
 		return "article/list";
 	}
 }
