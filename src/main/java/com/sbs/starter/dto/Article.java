f89337ff49b6bc7e462b41c5050770d4ddd649c4
LeeHungu4122 <ilopio@naver.com>
2019-09-08 15:51:32 +0900

    ArticleService, ArticleServiceImpl, ArticleDto 생성 및 세팅,getList() 함수를 구현(임시데이터로 리턴하도록 함.)

--- src/main/java/com/sbs/starter/dto/Article.java
+++ src/main/java/com/sbs/starter/dto/Article.java
@@ -1,7 +1,7 @@
 package com.sbs.starter.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Data
--- src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
+++ src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
@@ -10,7 +10,7 @@ import com.sbs.starter.dto.Article;
 @Service
 public class ArticleServiceImpl implements ArticleService {
 	public List<Article> getList() {
-		// 이건 가짜 데이터 이니다., 중간 확인용!!
+		// 확인용 임시 데이터.
 		Article article1 = new Article(1, "2019-08-20 12:12:12", "제목1", "내용1");
 		Article article2 = new Article(2, "2019-08-20 12:12:13", "제목2", "내용2");
 		Article article3 = new Article(3, "2019-08-20 12:12:14", "제목3", "내용3");
